e0ede1a6e2fd26eb1596171fb6ac204bcfc7615b
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-05-21 15:31:41 +0500

    Add files via upload

---
+++ Day 02/Question 04/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Question #04;
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look
+// something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(`Charlie Chaplin once said,"You will never find rainbows if 
+you are looking down."`);
---
+++ Day 02/Question 05/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 2: Famous Quote 2:
+// Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person.
+// Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, “A person who never made a mistake never tried anything new.”`;
+console.log(message);
