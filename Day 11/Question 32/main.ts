f179ce184510435f415fd177fc46e94c94e64e88
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-05-22 16:00:11 +0500

    Add files via upload

---
+++ Day 11/Question 32/main.ts
@@ -0,0 +1,36 @@
+/*Question # 32; Checking Usernames:
+ Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also 
+in the current_users list.
+• Loop through the new_users list to see if each new username has already been used. If it has, print a
+ message that the person will need to enter a new username. If a username has not been used, print a 
+ message saying that the username is available.
+• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+ */
+let current_users: string[] = [
+  "CathyPearl07",
+  "shutuphassaan",
+  "itsshani",
+  "Rose Mary",
+  "Rumeras",
+];
+let new_users: string[] = [
+  "Rumeras",
+  "Fahad ali",
+  "itsshani",
+  "whoishassnat",
+  "BadHunny",
+];
+
+new_users.forEach((new_user) => {
+  if (
+    current_users.some(
+      (current_user) => current_user.toLowerCase() === new_user.toLowerCase()
+    )
+  ) {
+    console.log(`${new_user} will need to enter a new username.`);
+  } else {
+    console.log(`${new_user} is available.`);
+  }
+});
