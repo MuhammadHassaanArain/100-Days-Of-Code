e7ddeb60b7d3beb38953646268ad305082265114
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-05-23 12:14:56 +0500

    Add files via upload

---
+++ Day 15/Question 44/main.js
@@ -0,0 +1,16 @@
+/**
+ Question # 44; Sandwiches:
+ Write a function that accepts a array of items a person wants on a sandwich. The function should have one
+ parameter that collects as many items as the function call provides, and it should print a summary of the
+ sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+ */
+function make_sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    console.log("Making a sandwich with: ".concat(items, "."));
+}
+make_sandwich("ham", "cheese");
+make_sandwich("turkey", "lettuce", "tomato");
+make_sandwich("avocado", "sprouts", "mustard", "mayo");
