de7de95a50039444e5e558fbca2b4a7c18f99f54
Hassaan Arain <161812835+MuhammadHassaanArain@users.noreply.github.com>
2024-05-27 12:06:30 +0500

    day-34-of-100-days-of-code

---
+++ Day 34/Question 100/main.js
@@ -0,0 +1,12 @@
+/*
+Question 100: Use the JavaScript Math object to find the square root of 144.
+
+Explain & TIP: The Math.sqrt() function is a straightforward way to calculate the square root of
+any positive number, directly providing the result you need.
+ */
+function a(num) {
+    // Finds the square root of 144 using Math.sqrt()
+    return Math.sqrt(num);
+}
+console.log(a(144)); // output 12
+// This line effectively calculates and shows the square root of 144.
---
+++ Day 34/Question 101/main.ts
@@ -0,0 +1,16 @@
+/*
+Question 101: Generate a random integer between 1 and 10.
+
+Explain & TIP: To generate a random integer within a specific range, you can use Math.random() in 
+combination with Math.floor() or Math.ceil() to ensure the result is an integer.
+ */
+
+
+// Generates a random integer between 1 and 10
+
+function getRandomInt(): number {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(getRandomInt()); // Outputs a random integer between 1 and 10
+// This function combines Math.random() with Math.floor() to create a random integer within our range.
